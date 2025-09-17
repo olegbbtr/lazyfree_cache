e78ba4317ff001eccfe4f8047ed26427cd4c81f4
Oleg Vasilev <oleg@bbtr.dev>
2025-09-17 16:52:27 +0000

    api cleanup

--- include/cache.h
+++ include/cache.h
@@ -1,16 +1,11 @@
 #ifndef CACHE_H
 #define CACHE_H
-
 #include <stdint.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <assert.h>
+#include <string.h>
 
-// PAGE_SIZE must be equal to kernel page size.
-#define PAGE_SIZE 4096
-
-
-// == Generic cache ==
 
 typedef struct lazyfree_cache* lazyfree_cache_t;
 typedef uint64_t lazyfree_key_t;
@@ -23,20 +18,39 @@ struct lazyfree_stats {
     size_t free_pages;
 };
 
-typedef struct {
-    // == INTERNAL USE ==
-    lazyfree_key_t _key; 
-    uint16_t _index;    
-    int8_t _chunk;      
-    // == END INTERNAL USE ==
 
-    uint8_t head;       // First byte of the page
-    uint8_t* tail;      // [1:PAGE_SIZE]
+// ========= Generic cache ============
+
+// PAGE_SIZE must be equal to kernel page size.
+#define PAGE_SIZE 4096
+
+// Use this to read from the page.
+typedef struct {
+    const uint8_t *head;     // [0:PAGE_SIZE-1]
+    uint8_t tail;            // last byte of the page
+
+    uint8_t __padding[15];
 } lazyfree_rlock_t;  
-static_assert(sizeof(lazyfree_rlock_t) == 24, "");
 
 // Use this to check if the entry is present in the cache.
-#define LOCK_ENTRY_FOUND(lock) ((lock)->tail != NULL)
+#define LAZYFREE_LOCK_IS_BLANK(lock) ((lock).head == NULL)
+
+// Use this to check if the lock is still valid.
+#define LAZYFREE_LOCK_IS_VALID(lock) ((lock).head[PAGE_SIZE-1] > 0)
+
+// Helper to memcpy with offset
+// Returns true if the lock is still valid.
+static inline bool lazyfree_read(void *dest, lazyfree_rlock_t lock, size_t offset, size_t size) {
+    if (LAZYFREE_LOCK_IS_BLANK(lock)) {
+        return false;
+    }
+    memcpy(dest, lock.head + offset, size);
+    if (offset + size == PAGE_SIZE) {
+        ((uint8_t*)dest)[size-1] = lock.tail;
+    }
+    return LAZYFREE_LOCK_IS_VALID(lock);
+}
+
 
 struct lazyfree_impl {
     lazyfree_cache_t (*new)(size_t cache_size, lazyfree_mmap_impl_t mmap_impl, lazyfree_madv_impl_t madv_impl);
@@ -44,13 +58,12 @@ struct lazyfree_impl {
 
     // == Optimistic Read Lock API ==
     lazyfree_rlock_t (*read_lock)(lazyfree_cache_t cache, lazyfree_key_t key);
-    bool             (*read_lock_check)(lazyfree_cache_t cache, lazyfree_rlock_t lock);
     void             (*read_unlock)(lazyfree_cache_t cache, lazyfree_rlock_t lock, bool drop);
 
     // == Write Lock API ==
     void* (*write_upgrade)(lazyfree_cache_t cache, lazyfree_rlock_t* lock);
     void* (*write_alloc)(lazyfree_cache_t cache, lazyfree_key_t key);
-    void     (*write_unlock)(lazyfree_cache_t cache, bool drop);
+    void  (*write_unlock)(lazyfree_cache_t cache, bool drop);
 
     // == Memory implementation ==
     lazyfree_mmap_impl_t mmap_impl;
@@ -64,26 +77,5 @@ struct lazyfree_impl lazyfree_impl();
 struct lazyfree_impl lazyfree_anon_impl();
 struct lazyfree_impl lazyfree_disk_impl();
 struct lazyfree_impl lazyfree_stub_impl();
-
-// == Memory allocation ==
-
-// Allocate anonymous memory.
-void *lazyfree_mmap_anon(size_t size);
-// Allocate file memory.
-void *lazyfree_mmap_file(size_t size);
-
-// MADV_FREE
-void lazyfree_madv_free(void *memory, size_t size);
-
-// MADV_COLD
-void lazyfree_madv_cold(void *memory, size_t size);
-
-// == Implementation details ==
-
-#define K 1024ul
-#define M (K*K)
-#define G (K*M)
-
-#define UNUSED(x) (void)(x)
 
 #endif
